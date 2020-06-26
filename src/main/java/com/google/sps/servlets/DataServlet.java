12e5960f3743133a7ea2e9dd955c54cc0dc5d53d
Ishani Santurkar <ishani.santurkar@gmail.com>
2020-06-26 21:50:56 +0000

    Add code to read graph from text file and print it out

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,11 @@
 
 package com.google.sps.servlets;
 
+import java.util.List;
+import java.util.Map;
+import proto.GraphProtos.Graph;
+import proto.GraphProtos.Node;
+import java.io.FileInputStream;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,7 +34,56 @@ public class DataServlet extends HttpServlet {
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    
+    /*
+     * Code to read graph from test.txt file in same directory and print it
+     * to the console
+     */
+    Graph graph = Graph.parseFrom(new FileInputStream("test.txt"));
+    List<String> roots = graph.getRootNameList();
+    System.out.println("Roots " + roots.toString());
+    Map<String, Node> nodesMap = graph.getNodesMap();
+    for(String nodeName : nodesMap.keySet()) {
+      System.out.print("Node " + nodeName);
+      Node thisNode = nodesMap.get(nodeName);
+      System.out.println(" has children: " + thisNode.getChildrenList().toString());
+    }
+    /*
+     * Code to build graph and write it to test.txt file in target directory
+     */
+    // Node.Builder nodeA = Node.newBuilder();
+    // nodeA.setName("A");
+    // nodeA.addChildren("B");
+
+    // Node.Builder nodeB = Node.newBuilder();
+    // nodeB.setName("B");
+    // nodeB.addChildren("D");
+
+    // Node.Builder nodeC = Node.newBuilder();
+    // nodeB.setName("C");
+
+    // Node.Builder nodeD = Node.newBuilder();
+    // nodeB.setName("D");
+
+    // Node.Builder nodeE = Node.newBuilder();
+    // nodeE.setName("E");
+    // nodeE.addChildren("F");
+
+    // Node.Builder nodeF = Node.newBuilder();
+    // nodeB.setName("F");
+
+    // Graph.Builder graph = Graph.newBuilder();
+    // graph.addRootName("A");
+    // graph.addRootName("E");
+    // graph.putNodesMap("A", nodeA.build());
+    // graph.putNodesMap("B", nodeB.build());
+    // graph.putNodesMap("C", nodeC.build());
+    // graph.putNodesMap("D", nodeD.build());
+    // graph.putNodesMap("E", nodeE.build());
+    // graph.putNodesMap("F", nodeF.build());
+
+    // FileOutputStream output = new FileOutputStream("test.txt");
+    // graph.build().writeTo(output);
+    // output.close();
   }
 
 
