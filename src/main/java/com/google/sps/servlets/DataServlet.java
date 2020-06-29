8ac3913eeee62bd313cf516f859a466af4cad19c
IshaniSanturkar <ishani.santurkar@gmail.com>
2020-06-29 13:31:08 -0700

    Store data in guava graph

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -22,6 +22,7 @@ import proto.GraphProtos.Graph;
 import proto.GraphProtos.Node;
 import proto.MutationProtos.MutationList;
 import proto.MutationProtos.Mutation;
+import proto.MutationProtos.TokenMutation;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -42,20 +43,18 @@ public class DataServlet extends HttpServlet {
      * Code to read graph from graph.txt file in same directory and print it to the
      * console, read mutations from mutations.txt and print it to console
      */
-    Graph inputGraph = Graph.parseFrom(getServletContext().getResourceAsStream("/WEB-INF/inputGraph.txt"));
+    Graph inputGraph = Graph.parseFrom(getServletContext().getResourceAsStream("/WEB-INF/graph.txt"));
 
     // Guava Graph
     MutableGraph<Node> graph = GraphBuilder.directed().build();
 
-    List<String> roots = inputGraph.getRootNameList();
-    System.out.println("Roots " + roots.toString());
+    List<String> roots = inputGraph.getRootNameList();\
     Map<String, Node> nodesMap = inputGraph.getNodesMapMap();
     for (String nodeName : nodesMap.keySet()) {
       
-      // Guava graph
-      graph.addNode(nodesMap.get(nodeName));
-      System.out.print("Node " + nodeName);
       Node thisNode = nodesMap.get(nodeName);
+      // Guava graph
+      graph.addNode(thisNode);
 
       // Add edges to the guava Graph
       for (String child : thisNode.getChildrenList()) {
@@ -66,100 +65,40 @@ public class DataServlet extends HttpServlet {
 
     List<Mutation> mutList = MutationList.parseFrom(getServletContext().getResourceAsStream("/WEB-INF/mutations.txt"))
         .getMutationList();
-    for (Mutation m : mutList) {
-      System.out.println("Type " + m.getType());
-      System.out.println("Start Node " + m.getStartNode());
-      System.out.println("End Node " + m.getEndNode());
+
+    for (Mutation mut : mutList) {
+      Node startNode = mut.getStartNode();
+      Node endNode = mut.getEndNode();
+      switch (mut.getType()) {
+        case ADD_NODE:
+        graph.addNode(startNode);
+        for (String child : startNode.getChildrenList()) {
+          graph.putEdge(startNode, nodesMap.get(child));
+        }
+          break;
+        case ADD_EDGE:
+        graph.putEdge(startNode, endNode);
+          break;
+        case DELETE_NODE:
+        graph.removeNode(startNode);
+          break;
+        case DELETE_EDGE:
+        graph.removeEdge(startNode, endNode);
+          break;
+        case CHANGE_TOKEN:
+          changeNodeToken(graph, startNode, mut.getTokenChange());
+          break;
+        default:
+          break;
+      }
     }
-    /*
-     * Code to build graph and write it to graph.txt file in target directory and
-     * build graph and write it to mutations.txt in the target directory
-     */
-    // Node.Builder nodeA = Node.newBuilder();
-    // nodeA.setName("A");
-    // nodeA.addChildren("B");
-    // nodeA.addChildren("C");
+  }
 
-    // Node.Builder nodeB = Node.newBuilder();
-    // nodeB.setName("B");
-    // nodeB.addChildren("D");
-
-    // Node.Builder nodeC = Node.newBuilder();
-    // nodeC.setName("C");
-
-    // Node.Builder nodeD = Node.newBuilder();
-    // nodeD.setName("D");
-
-    // Node.Builder nodeE = Node.newBuilder();
-    // nodeE.setName("E");
-    // nodeE.addChildren("F");
-
-    // Node.Builder nodeF = Node.newBuilder();
-    // nodeF.setName("F");
-
-    // Node.Builder nodeG = Node.newBuilder();
-    // nodeG.setName("G");
-
-    // Node.Builder nodeH = Node.newBuilder();
-    // nodeH.setName("H");
-
-    // Graph.Builder graph = Graph.newBuilder();
-    // graph.addRootName("A");
-    // graph.addRootName("E");
-    // graph.putNodesMap("A", nodeA.build());
-    // graph.putNodesMap("B", nodeB.build());
-    // graph.putNodesMap("C", nodeC.build());
-    // graph.putNodesMap("D", nodeD.build());
-    // graph.putNodesMap("E", nodeE.build());
-    // graph.putNodesMap("F", nodeF.build());
-
-    // Mutation.Builder mutationA = Mutation.newBuilder();
-    // // Delete node
-    // mutationA.setTypeValue(2);
-    // mutationA.setStartNode(nodeF.build());
-
-    // Mutation.Builder mutationB = Mutation.newBuilder();
-    // // Add node
-    // mutationB.setTypeValue(0);
-    // mutationB.setStartNode(nodeG.build());
-
-    // Mutation.Builder mutationB1 = Mutation.newBuilder();
-    // // Add edge from E to G
-    // mutationB1.setTypeValue(1);
-    // mutationB1.setStartNode(nodeE.build());
-    // mutationB1.setEndNode(nodeG.build());
-
-    // Mutation.Builder mutationC = Mutation.newBuilder();
-    // // Add node
-    // mutationC.setTypeValue(0);
-    // mutationC.setStartNode(nodeH.build());
-
-    // Mutation.Builder mutationC1 = Mutation.newBuilder();
-    // mutationC1.setTypeValue(1);
-    // mutationC1.setStartNode(nodeH.build());
-    // mutationC1.setEndNode(nodeG.build());
-
-    // Mutation.Builder mutationD = Mutation.newBuilder();
-    // // Add edge
-    // mutationD.setTypeValue(1);
-    // mutationD.setStartNode(nodeD.build());
-    // mutationD.setEndNode(nodeG.build());
-
-    // MutationList.Builder mutationList = MutationList.newBuilder();
-    // mutationList.addMutation(mutationA.build());
-    // mutationList.addMutation(mutationB.build());
-    // mutationList.addMutation(mutationB1.build());
-    // mutationList.addMutation(mutationC.build());
-    // mutationList.addMutation(mutationC1.build());
-    // mutationList.addMutation(mutationD.build());
-
-    // FileOutputStream output = new FileOutputStream("graph.txt");
-    // graph.build().writeTo(output);
-    // output.close();
-
-    // FileOutputStream output1 = new FileOutputStream("mutations.txt");
-    // mutationList.build().writeTo(output1);
-    // output1.close();
+  private void changeNodeToken(MutableGraph<Node> graph, Node node, TokenMutation tokenMut) {
+    String tokenName = tokenMut.getTokenName();
+    // Add token
+    if(tokenMut.getTypeValue() == 0) {
+    }
   }
 
   /*
