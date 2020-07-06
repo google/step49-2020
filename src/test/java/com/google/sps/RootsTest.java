89f18858282ef32be1b6e0ed2844043a55df7d08
Cindy Hao <cindyhao@google.com>
2020-07-06 19:56:34 +0000

    add licensing and fix comments

--- src/test/java/com/google/sps/RootsTest.java
+++ src/test/java/com/google/sps/RootsTest.java
@@ -1,10 +1,22 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.sps;
 
 import com.google.common.graph.*;
 import com.google.sps.servlets.DataServlet;
-import java.util.Set;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.HashSet;
 import com.google.sps.data.GraphNode;
 import com.proto.GraphProtos.Node;
@@ -270,7 +282,7 @@ public class RootsTest {
     HashSet<String> roots = new HashSet<>();
 
     servlet.graphFromProtoNodes(protoNodesMap, graph, graphNodesMap, roots);
-    // After mutation
+    // Before mutation
     Assert.assertEquals(roots.size(), 1);
     Assert.assertTrue(roots.contains("A"));
 
