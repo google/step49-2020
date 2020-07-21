fb8104d6904878a5414a70d2a4a306da058b3d4d
Cindy Hao <cindyhao@google.com>
2020-07-21 22:17:51 +0000

    update utility to include these changes

--- src/main/java/com/google/sps/Utility.java
+++ src/main/java/com/google/sps/Utility.java
@@ -116,7 +116,7 @@ public final class Utility {
           }
         }
       }
-      return DataGraph.create(curr.graph(), curr.graphNodesMap(), curr.roots(), mutationNum);
+      return DataGraph.create(curr.graph(), curr.graphNodesMap(), curr.roots(), mutationNum, curr.tokenMap());
     } else {
       // Create a copy of the original graph and start from the original graph
       DataGraph originalCopy = original.getCopy();
@@ -131,7 +131,7 @@ public final class Utility {
         }
       }
       return DataGraph.create(
-          originalCopy.graph(), originalCopy.graphNodesMap(), originalCopy.roots(), mutationNum);
+          originalCopy.graph(), originalCopy.graphNodesMap(), originalCopy.roots(), mutationNum, original.tokenMap());
     }
   }
 
