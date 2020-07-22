1a11c3c93e6af9a4b02a52764dc8d8fb4cf571f5
Cindy Hao <cindyhao@google.com>
2020-07-22 05:07:55 +0000

    write function that merges multiple lists

--- src/main/java/com/google/sps/Utility.java
+++ src/main/java/com/google/sps/Utility.java
@@ -17,6 +17,7 @@ package com.google.sps;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -39,8 +40,8 @@ public final class Utility {
   }
 
   /**
-   * Converts a proto node object into a graph node object that does not store the names of the
-   * child nodes but may store additional information.
+   * Converts a proto node object into a graph node object that does not store the
+   * names of the child nodes but may store additional information.
    *
    * @param thisNode the input data Node object
    * @return a useful node used to construct the Guava Graph
@@ -53,50 +54,47 @@ public final class Utility {
   }
 
   /**
-   * Converts a Guava graph into a String encoding of a JSON Object. The object contains nodes and
-   * edges of the graph.
+   * Converts a Guava graph into a String encoding of a JSON Object. The object
+   * contains nodes and edges of the graph.
    *
-   * @param graph the graph to convert into a JSON String
+   * @param graph        the graph to convert into a JSON String
    * @param maxMutations the length of the list of mutations
-   * @param mutDiff the difference between the current graph and the requested graph
-   * @return a JSON object containing as entries the nodes and edges of this graph as well as the
-   *     length of the list of mutations this graph is an intermediate result of applying
+   * @param mutDiff      the difference between the current graph and the
+   *                     requested graph
+   * @return a JSON object containing as entries the nodes and edges of this graph
+   *         as well as the length of the list of mutations this graph is an
+   *         intermediate result of applying
    */
-  public static String graphToJson(
-      MutableGraph<GraphNode> graph, List<Integer> mutationIndices, MultiMutation mutDiff) {
-    Type typeOfNode = new TypeToken<Set<GraphNode>>() {}.getType();
-    Type typeOfEdge = new TypeToken<Set<EndpointPair<GraphNode>>>() {}.getType();
-    Type typeOfIndices = new TypeToken<List<Integer>>() {}.getType();
+  public static String graphToJson(MutableGraph<GraphNode> graph, List<Integer> mutationIndices,
+      MultiMutation mutDiff) {
+    Type typeOfNode = new TypeToken<Set<GraphNode>>() {
+    }.getType();
+    Type typeOfEdge = new TypeToken<Set<EndpointPair<GraphNode>>>() {
+    }.getType();
+    Type typeOfIndices = new TypeToken<List<Integer>>() {
+    }.getType();
     Gson gson = new Gson();
     String nodeJson = gson.toJson(graph.nodes(), typeOfNode);
     String edgeJson = gson.toJson(graph.edges(), typeOfEdge);
-    String mutDiffJson =
-        (mutDiff == null || !mutDiff.isInitialized()) ? "" : gson.toJson(mutDiff.getMutationList());
+    String mutDiffJson = (mutDiff == null || !mutDiff.isInitialized()) ? "" : gson.toJson(mutDiff.getMutationList());
     String reason = (mutDiff == null || !mutDiff.isInitialized()) ? "" : mutDiff.getReason();
     String mutationIndicesJson = gson.toJson(mutationIndices, typeOfIndices);
-    String resultJson =
-        new JSONObject()
-            .put("nodes", nodeJson)
-            .put("edges", edgeJson)
-            .put("mutationDiff", mutDiffJson)
-            .put("reason", reason)
-            .put("mutationIndices", mutationIndicesJson)
-            .toString();
+    String resultJson = new JSONObject().put("nodes", nodeJson).put("edges", edgeJson).put("mutationDiff", mutDiffJson)
+        .put("reason", reason).put("mutationIndices", mutationIndicesJson).toString();
     return resultJson;
   }
 
   /**
-   * @param original the original graph
-   * @param curr the current (most recently-requested) graph (requires that original != curr)
-   * @param mutationNum number of mutations to apply
+   * @param original     the original graph
+   * @param curr         the current (most recently-requested) graph (requires
+   *                     that original != curr)
+   * @param mutationNum  number of mutations to apply
    * @param multiMutList multi-mutation list
    * @return the resulting data graph or null if there was an error
    */
-  public static DataGraph getGraphAtMutationNumber(
-      DataGraph original, DataGraph curr, int mutationNum, List<MultiMutation> multiMutList)
-      throws IllegalArgumentException {
-    Preconditions.checkArgument(
-        original != curr, "The current graph and the original graph refer to the same object");
+  public static DataGraph getGraphAtMutationNumber(DataGraph original, DataGraph curr, int mutationNum,
+      List<MultiMutation> multiMutList) throws IllegalArgumentException {
+    Preconditions.checkArgument(original != curr, "The current graph and the original graph refer to the same object");
 
     if (mutationNum < -1) {
       return null;
@@ -116,8 +114,7 @@ public final class Utility {
           }
         }
       }
-      return DataGraph.create(
-          curr.graph(), curr.graphNodesMap(), curr.roots(), mutationNum, curr.tokenMap());
+      return DataGraph.create(curr.graph(), curr.graphNodesMap(), curr.roots(), mutationNum, curr.tokenMap());
     } else {
       // Create a copy of the original graph and start from the original graph
       DataGraph originalCopy = original.getCopy();
@@ -131,23 +128,23 @@ public final class Utility {
           }
         }
       }
-      return DataGraph.create(
-          originalCopy.graph(),
-          originalCopy.graphNodesMap(),
-          originalCopy.roots(),
-          mutationNum,
+      return DataGraph.create(originalCopy.graph(), originalCopy.graphNodesMap(), originalCopy.roots(), mutationNum,
           original.tokenMap());
     }
   }
 
   /**
-   * Returns a multi-mutation (list of mutations) that need to be applied to get from the graph at
-   * currIndex to the graph at nextIndex as long as nextIndex = currIndex + 1
+   * Returns a multi-mutation (list of mutations) that need to be applied to get
+   * from the graph at currIndex to the graph at nextIndex as long as nextIndex =
+   * currIndex + 1
    *
-   * @param multiMutList the list of multi-mutations that are to be applied to the initial graph
-   * @param index the index in the above list at which the multimutation to apply is
-   * @return a multimutation with all the changes to apply to the current graph to get the next
-   *     graph or null if the provided indices are out of bounds or non-consecutive
+   * @param multiMutList the list of multi-mutations that are to be applied to the
+   *                     initial graph
+   * @param index        the index in the above list at which the multimutation to
+   *                     apply is
+   * @return a multimutation with all the changes to apply to the current graph to
+   *         get the next graph or null if the provided indices are out of bounds
+   *         or non-consecutive
    */
   public static MultiMutation getDiffBetween(List<MultiMutation> multiMutList, int index) {
     if (index < 0 || index >= multiMutList.size()) {
@@ -155,6 +152,7 @@ public final class Utility {
     }
     return multiMutList.get(index);
   }
+
   /**
    * Returns a list of the indices of the relevant
    *
@@ -162,8 +160,7 @@ public final class Utility {
    * @param origList the original list of mutations
    * @return a list of indices that are relevant to the node
    */
-  public static ArrayList<Integer> getMutationIndicesOfNode(
-      String nodeName, List<MultiMutation> origList) {
+  public static ArrayList<Integer> getMutationIndicesOfNode(String nodeName, List<MultiMutation> origList) {
     ArrayList<Integer> lst = new ArrayList<>();
     // Shouldn't happen, but in case the nodeName is null an empty list is returned
     if (nodeName == null) {
@@ -185,10 +182,11 @@ public final class Utility {
   }
 
   /**
-   * Finds the INDEX of the element in searchList that is strictly GREATER than tgt in a SORTED list
+   * Finds the INDEX of the element in searchList that is strictly GREATER than
+   * tgt in a SORTED list
    *
    * @param searchList a list of integers to search through. Assumes it's sorted
-   * @param tgt the number to find the next biggest number from
+   * @param tgt        the number to find the next biggest number from
    * @return the INDEX of the next greater number. -1 if none
    */
   public static int getNextGreatestNumIndex(List<Integer> searchList, int tgt) {
@@ -208,13 +206,14 @@ public final class Utility {
         end = mid - 1;
       }
     }
-    if (ans == -1) return -1;
+    if (ans == -1)
+      return -1;
     return ans;
   }
 
   /**
-   * Converts a Guava graph containing nodes of type GraphNode into a set of names of nodes
-   * contained in the graph
+   * Converts a Guava graph containing nodes of type GraphNode into a set of names
+   * of nodes contained in the graph
    *
    * @param graph the graph to return node names for
    * @return a set of names of nodes in the graph
@@ -223,15 +222,86 @@ public final class Utility {
     return graph.nodes().stream().map(node -> node.name()).collect(Collectors.toSet());
   }
 
+  public static List<Integer> mergeSortedLists(List<List<Integer>> sortedLists) {
+    int numLists = sortedLists.size();
+    if (sortedLists.size() == 0)
+      return null;
+    else if (sortedLists.size() == 1) {
+      return sortedLists.get(0);
+    } else {
+      int left = numLists / 2;
+      return mergeTwoLists(mergeSortedLists(new ArrayList<>(sortedLists.subList(0, left))),
+          mergeSortedLists(new ArrayList<>(sortedLists.subList(left, sortedLists.size()))));
+    }
+
+    // We merge two lists at a time until we get one list remaining
+    // while (numLists != 1) {
+    // int low = 0;
+    // int high = numLists - 1;
+    // while (low < high) {
+    // // System.out.println(mergeTwoLists(sortedLists.get(low),
+    // sortedLists.get(high)));
+    // System.out.println(sortedLists);
+    // sortedLists.set(low, mergeTwoLists(sortedLists.get(low),
+    // sortedLists.get(high)));
+    // low++;
+    // high--;
+    // }
+    // numLists = high + 1;
+    // }
+    // return sortedLists.get(0);
+  }
+
+  public static List<Integer> mergeTwoLists(List<Integer> l1, List<Integer> l2) {
+    System.out.println(l1);
+    System.out.println(l2);
+    ArrayList<Integer> result = new ArrayList<>();
+    int pointer1 = 0;
+    int pointer2 = 0;
+
+    while (pointer1 < l1.size() && pointer2 < l2.size()) {
+      int smallest;
+      if (l1.get(pointer1) <= l2.get(pointer2)) {
+        smallest = l1.get(pointer1);
+        pointer1++;
+      } else {
+        smallest = l2.get(pointer2);
+        pointer2++;
+      }
+      // no duplicates
+      if (result.size() == 0 || result.get(result.size() - 1) != smallest) {
+        result.add(smallest);
+      }
+    }
+    System.out.println(pointer1);
+    System.out.println(pointer2);
+    // Check individual, if the other has reached the bounds
+    while (pointer1 < l1.size()) {
+      System.out.println("hello");
+      if (result.size() == 0 || result.get(result.size() - 1) != l1.get(pointer1)) {
+        result.add(l1.get(pointer1));
+      }
+      pointer1++;
+    }
+    while (pointer2 < l2.size()) {
+      if (result.size() == 0 || result.get(result.size() - 1) != l2.get(pointer2)) {
+        result.add(l2.get(pointer2));
+      }
+      pointer2++;
+    }
+    System.out.println(result);
+    return result;
+  }
+
   /**
-   * Filters the mutations contained in this multimutation to be only the ones that affect the nodes
-   * in the provided set
+   * Filters the mutations contained in this multimutation to be only the ones
+   * that affect the nodes in the provided set
    *
-   * @param mm the multimutation to filter
+   * @param mm        the multimutation to filter
    * @param nodeNames the list of node names to return perninent mutations for
-   * @return a multimutation containing only those mutations in mm affecting nodes in nodeNames,
-   *     null if the multimutation is null and the multimutation itself if there is no name to
-   *     filter by
+   * @return a multimutation containing only those mutations in mm affecting nodes
+   *         in nodeNames, null if the multimutation is null and the multimutation
+   *         itself if there is no name to filter by
    */
   public static MultiMutation filterMultiMutationByNodes(MultiMutation mm, Set<String> nodeNames) {
     if (mm == null || nodeNames.size() == 0) {
@@ -246,9 +316,6 @@ public final class Utility {
         filteredMutationList.add(mut);
       }
     }
-    return MultiMutation.newBuilder()
-        .addAllMutation(filteredMutationList)
-        .setReason(mm.getReason())
-        .build();
+    return MultiMutation.newBuilder().addAllMutation(filteredMutationList).setReason(mm.getReason()).build();
   }
 }
