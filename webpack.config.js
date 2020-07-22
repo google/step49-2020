dd4a43945dc9dc8e0c003c2be3d4b84843021178
IshaniSanturkar <ishani.santurkar@gmail.com>
2020-07-22 13:13:03 -0700

    Add slider element and sass dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const autoprefixer = require('autoprefixer');
 
 // webpack.config.js
 module.exports = {
   mode: 'development',
-  entry: './src/main/webapp/script.js',
+  entry: ["./src/main/webapp/style.scss", "./src/main/webapp/script.js"],
   output: {
     filename: 'main.js',
     path: path.join(__dirname, '/src/main/webapp/dist'),
@@ -14,14 +15,32 @@ module.exports = {
     rules: [
       { test: /\.css$/, loader: "style-loader!css-loader" },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.scss$/,
         use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              webpackImporter: false,
+              sassOptions: {
+                includePaths: ['./node_modules'],
+              },
+            },
+          }
         ],
       },
     ]
